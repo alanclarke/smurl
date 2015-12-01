753b72785322654694a839594fbdd39f0cb60bb3
alanclarke <hi@alz.so>
2015-12-01 14:13:31 +0000

    use urlite, add coverage, switch to webpack and add makefile

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,16 @@
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['mocha', 'browserify', 'expect', 'sinon'],
-    files: [
-      'test/*.js'
-    ],
-    exclude: [
-      'karma.conf.js'
-    ],
-    preprocessors: {
-      'test/*.js': ['browserify'],
-      'lib/*.js': ['browserify']
+    frameworks: ['mocha'],
+    files: [ 'test/**/test-*' ],
+    preprocessors: { 'test/**/test-*': ['webpack', 'sourcemap'] },
+    webpack: {
+      watch: true,
+      devtool: 'inline-source-map'
     },
-    browserify: {
-      debug: true
+    webpackServer: {
+      quiet: true,
+      noInfo: true
     },
-    reporters: ['spec'],
-    logLevel: config.LOG_INFO,
     browsers: ['Chrome']
   })
 }
